16cb38a12a225a49bf0b5a434ad5240de7a49b3a
C-NASIR <cnasir22.0@gmail.com>
2022-05-19 14:12:04 -0500

    check for the custom string length and remove teh console logs

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -5,21 +5,16 @@ const Link = require("../models/link");
 module.exports = {
   createLink: async function ({ userInput }, req) {
     const randomChars = randomString.generate(7);
-    console.log("++===============+==", userInput);
     let possibleSlug = "http://localhost:3001/" + userInput.slug ?? randomChars;
 
-    console.log("=================================", possibleSlug);
     const exists = await Link.findAll({
       where: {
         slug: possibleSlug,
       },
     });
 
-    console.log("Does it exists", exists);
-
-    if (exists.length > 0)
+    if (exists.length > 0 || userInput.slug.length < 4)
       possibleSlug = "http://localhost:3001/" + randomString.generate(7);
-    else console.log("it doesn't exists");
 
     const result = await Link.create({
       text: userInput.link,
