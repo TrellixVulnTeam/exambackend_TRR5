f2326e9cb086541b55f9160efd9d69c3200aa587
C-NASIR <cnasir22.0@gmail.com>
2022-05-19 13:16:19 -0500

    Added appropriate headers, so we can easily call it from another application

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const { graphqlHTTP } = require("express-graphql");
 
 const graphqlResolver = require("./graphql/resolvers");
@@ -10,6 +11,21 @@ sequelize
   .then(() => console.log("db ready "))
   .catch((err) => console.log("error connecting to the database", err));
 const app = express();
+
+app.use(bodyParser.json()); // application/json
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use(
   "/graphql",
